d49f4ec3d0db60d19917abbd0f446857452bdbf0
Ruvindi Dilsara <daruvindid@gmail.com>
2024-11-01 04:31:33 +0530

    Add more validation criteria for the priority

--- src/main/java/irusri/assignment/todo_app/dtos/TodoRequest.java
+++ src/main/java/irusri/assignment/todo_app/dtos/TodoRequest.java
@@ -1,6 +1,8 @@
 package irusri.assignment.todo_app.dtos;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -10,7 +12,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 
 @Data
-@NoArgsConstructor
+//@NoArgsConstructor
 @AllArgsConstructor
 public class TodoRequest {
     @NotBlank(message = "title is required")
@@ -21,7 +23,11 @@ public class TodoRequest {
     @NotNull(message = "dueDate is required")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate dueDate;
+
     @NotNull(message = "priority is required")
+    @Min(value = 1, message = "Priority must be at least 1")
+    @Max(value = 10, message = "Priority must be at most 10")
     private Integer priority;
+
     private boolean completed;
 }
